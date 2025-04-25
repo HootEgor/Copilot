94933d91c324ca454ad826bf51290405986869ab
GitHub Action <action@github.com>
2025-04-25 10:54:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/hootegor/copilot/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/hootegor/copilot/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.hootegor.copilot.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
